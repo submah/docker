fc000c1620e88d53494035c56e221025a9ce2ebc
submah <mahapatra.subratkumar@gmail.com>
2020-10-26 20:40:51 +0530

    Update server.php
    updated row 63 from 1 to 0

--- app/server.php
+++ app/server.php
@@ -60,7 +60,7 @@
 			$query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
 			$results = mysqli_query($db, $query);
 
-			if (mysqli_num_rows($results) == 1) {
+			if (mysqli_num_rows($results) == 0) {
 				$_SESSION['username'] = $username;
 				$_SESSION['success'] = "You are now logged in";
 				header('location: index.php');
